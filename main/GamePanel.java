e2531fde7d9d352bf583178ef78c14b849229282
Ashokpal24 <ashok1717pal@gmail.com>
2022-06-26 20:51:21 +0530

    image loading and render update

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,20 +1,41 @@
 package main;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import inputs.KeyBoardInput;
 import utils.Vector2d;
-
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 import java.awt.Graphics;
+import java.io.InputStream;
 public class GamePanel extends JPanel {
     private Vector2d vel=new Vector2d(0.0f, 0.0f);
+    private BufferedImage img,subImg;
+    
     public GamePanel(){
         addKeyListener(new KeyBoardInput(this));
+        imageImport();
+
+        Dimension size=new Dimension(1280,720);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setPreferredSize(size);
     }
     public void setvelocity(Float x,Float y){
         this.vel.x+=x;
         this.vel.y+=y;
     }
+    private void imageImport()
+    {
+        InputStream is=getClass().getResourceAsStream("/res/player.png");
+        try {
+            img=ImageIO.read(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(Math.round(this.vel.x),Math.round(this.vel.y), 200, 50);
+        subImg=img.getSubimage(1*32, 0, 32, 32);
+        g.drawImage(subImg,Math.round(this.vel.x),Math.round(this.vel.y),64, 64, null);
     }
 }
--- main/GameWindow.java
+++ main/GameWindow.java
@@ -2,9 +2,11 @@ package main;
 import javax.swing.JFrame;
 public class GameWindow extends JFrame {
     public GameWindow(GamePanel gamePanel){
-        setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(gamePanel);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        pack();
         setVisible(true);
     }   
 }
